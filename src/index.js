ce7a696fd107d6da1e7c64dcd443b163d306e48b
guskirb <anguskirby1998@gmail.com>
2024-04-16 23:34:16 +0100

    Refactor render code into class

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import '../src/style.css';
 import newProject from '../src/project.js';
-import project from '../src/project.js';
 
 const sidebar = document.getElementById("projectList");
 const projectInputWindow = document.getElementById("addProjectPopup");
@@ -19,8 +18,7 @@ const editTaskDesc = document.getElementById("editTaskDesc");
 const editDeadline = document.getElementById("editDeadline");
 const editPriority = document.getElementById("editUrgency");
 const editPopup = document.getElementById("editList");
-const closeEditButton = document.getElementById("closeEdit")
-const editLower = document.getElementById("editLower");
+const closeEditButton = document.getElementById("closeEdit");
 const addListPopup = document.getElementById("details");
 const addListButton = document.getElementById("addList");
 const closeAddButton = document.getElementById("closeDetails");
@@ -34,21 +32,105 @@ let currentProject;
 
 let today = new newProject("Today");
 let thisWeek = new newProject("This Week");
-thisWeek.addToList("Work Meeting", "high", "Bring draft proposal", "2024-04-30")
-today.addToList("Go shopping", "low", "Buy apples");
+thisWeek.addToList("Mum's birthday", "medium", "Remember to buy her a gift", "2024-04-29");
+thisWeek.addToList("Work meeting", "high", "Bring draft proposal", "2024-04-30");
 today.addToList("Meet friends for lunch", "low", "Call her 10 minutes before");
+today.addToList("Go shopping", "medium", "Buy apples");
+today.addToList("Make dinner", "low", "Look up pie recipe");
 projectList.push(today);
 projectList.push(thisWeek);
 
+let render = new class RenderClass {
+
+    renderList(project, value) {
+        let newDiv = document.createElement("div");
+        let deleteButton = document.createElement("img");
+        let editButton = document.createElement("img");
+        let checkBox = document.createElement("input")
+        checkBox.type = "checkbox";
+        projectTitle.textContent = project.name;
+        listMain.appendChild(newDiv);
+        listMain.lastChild.classList.add(value);
+        newDiv.appendChild(checkBox);
+        newDiv.appendChild(document.createElement("li")).textContent = project.toDoList[value].name;
+        newDiv.appendChild(document.createElement("li")).textContent = project.toDoList[value].date;
+        newDiv.lastChild.classList.add("date");
+        newDiv.appendChild(document.createElement("div"))
+        newDiv.lastChild.classList.add(project.toDoList[value].urgency);
+        newDiv.appendChild(editButton);
+        editButton.classList.add("edit");
+        editButton.addEventListener("click", () => {
+            editEventListener(project, value);
+        })
+        newDiv.appendChild(deleteButton);
+        deleteButton.classList.add("delete");
+        deleteButton.addEventListener("click", () => {
+            deleteEventListener(listMain, newDiv, project, value);
+        })
+    }
+
+    newListButton() {
+        if (currentProject === undefined) {
+            return alert("Please Select a List");
+        } if (taskName.value === "") {
+            alert("Please Enter a Task Name");
+        } else {
+            currentProject.addToList(taskName.value, priority.value, taskDesc.value, deadline.value);
+            render.refreshList(currentProject);
+        }
+    }
+
+    projectButtons() {
+        this.clearButtons();
+        for (let x = 0; x < projectList.length; x++) {
+            this.newProjectButton(x);
+        }
+    }
+
+    newProjectButton(value) {
+        let newDiv = document.createElement("div");
+        let newButton = document.createElement("button");
+        let deleteButton = document.createElement("img");
+        sidebar.appendChild(newDiv);
+        newDiv.appendChild(newButton).textContent = projectList[value].projectName;
+        newDiv.appendChild(deleteButton);
+        deleteButton.classList.add("projectDelete")
+        projectEventListeners(newDiv, value);
+    }
+
+    deleteProject(value) {
+        projectList.splice(value, 1);
+        render.projectButtons();
+    }
+
+    refreshList(project) {
+        this.clearList();
+        for (let x = 0; x < project.toDoList.length; x++) {
+            render.renderList(project, x);
+        }
+    }
+
+    clearList() {
+        while (listMain.firstChild) {
+            listMain.removeChild(listMain.lastChild);
+        }
+    }
+
+    clearButtons() {
+        while (sidebar.firstChild) {
+            sidebar.removeChild(sidebar.lastChild);
+        }
+    }
+
+}
+
 searchBar.addEventListener("input", e => {
-    clearList();
+    render.clearList();
     const value = e.target.value.toLowerCase();
     console.log(currentProject);
     for (let x = 0; x < currentProject.toDoList.length; x++) {
         if (currentProject.toDoList[x].item.toLowerCase().includes(value)) {
-            renderList(currentProject, x);
-        } else {
-
+            render.renderList(currentProject, x);
         }
     }
 })
@@ -70,18 +152,9 @@ addListButton.addEventListener("click", () => {
     }
 })
 
-function newListButton() {
-    if (currentProject === undefined) {
-        return alert("Please Select a List");
-    } if (taskName.value === "") {
-        alert("Please Enter a Task Name")
-    } else {
-        currentProject.addToList(taskName.value, priority.value, taskDesc.value, deadline.value);
-        refreshList(currentProject);
-    }
-}
 
-submitButton.addEventListener("click", newListButton);
+
+submitButton.addEventListener("click", render.newListButton);
 
 // add project popup window
 showInputButton.addEventListener("click", () => {
@@ -99,87 +172,31 @@ popupCloseButton.addEventListener("click", () => {
 // add a new project to list
 addButton.addEventListener("click", () => {
     if (projectInput.value === "") {
-        return;
+        alert("Please Enter a Project Name");
     } else {
         projectList.push(new newProject(projectInput.value));
-        projectButtons();
+        render.projectButtons();
         projectInput.value = "";
     }
 })
 
-//remove all project buttons
-function projectButtons() {
-    clearButtons();
-    for (let x = 0; x < projectList.length; x++) {
-        newProjectButton(x);
-    }
-}
-
-//add a new project button
-function newProjectButton(value) {
-    let newDiv = document.createElement("div");
-    let newButton = document.createElement("button");
-    let deleteButton = document.createElement("img");
-    sidebar.appendChild(newDiv);
-    newDiv.appendChild(newButton).textContent = projectList[value].projectName;
-    newDiv.appendChild(deleteButton);
-    deleteButton.classList.add("projectDelete")
-    projectEventListeners(newDiv, value);
-}
 
 function projectEventListeners(element, value) {
+
     element.firstChild.addEventListener("click", () => {
-        refreshList(projectList[value]);
+        document.querySelector(".select")?.classList.remove("select");
+        element.classList.add("select");
+        render.refreshList(projectList[value]);
         currentProject = projectList[value];
     });
     element.lastChild.addEventListener("click", () => {
-        deleteProject(value)
+        render.deleteProject(value)
     });
-}
-
-function deleteProject(value) {
-    projectList.splice(value, 1);
-    projectButtons();
-}
-
-function refreshList(project) {
-    clearList();
-    for (let x = 0; x < project.toDoList.length; x++) {
-        renderList(project, x);
-    }
-}
-
-function renderList(project, value) {
-    let newDiv = document.createElement("div");
-    let deleteButton = document.createElement("img");
-    let editButton = document.createElement("img");
-    let checkBox = document.createElement("input")
-    checkBox.type = "checkbox";
-
-    projectTitle.textContent = project.name;
-    listMain.appendChild(newDiv);
-    listMain.lastChild.classList.add(value);
-    newDiv.appendChild(checkBox);
-    newDiv.appendChild(document.createElement("li")).textContent = project.toDoList[value].name;
-    newDiv.appendChild(document.createElement("li")).textContent = project.toDoList[value].date;
-    newDiv.lastChild.classList.add("date");
-    newDiv.appendChild(document.createElement("div"))
-    newDiv.lastChild.classList.add(project.toDoList[value].urgency);
-    newDiv.appendChild(editButton);
-    editButton.classList.add("edit");
-    editButton.addEventListener("click", () => {
-        editEventListener(project, value);
-    })
-    newDiv.appendChild(deleteButton);
-    deleteButton.classList.add("delete");
-    deleteButton.addEventListener("click", () => {
-        deleteEventListener(listMain, newDiv, project, value);
-    })
 }
 
 function editEventListener(project, value) {
     editPopup.removeChild(editPopup.lastChild);
-    editPopup.appendChild(document.createElement("button")).textContent = "Submit";
+    editPopup.appendChild(document.createElement("button")).textContent = "Edit";
     editPopup.style.display = "flex";
     editPopup.style.gridRow = "2 / 3";
     editPopup.style.gridColumn = "3 / 4";
@@ -194,32 +211,23 @@ function editEventListener(project, value) {
 }
 
 function editButtonListener(project, value) {
-    project.toDoList[value].name = editTaskName.value;
-    project.toDoList[value].note = editTaskDesc.value;
-    project.toDoList[value].date = editDeadline.value;
-    project.toDoList[value].urgency = editPriority.value;
-    editPopup.style.display = "none";
-    addListPopup.style.gridRow = "2 / 4";
-    refreshList(currentProject);
+    if (editTaskName.value === "") {
+        alert("Please Enter a Task Name");
+    } else {
+        project.toDoList[value].name = editTaskName.value;
+        project.toDoList[value].note = editTaskDesc.value;
+        project.toDoList[value].date = editDeadline.value;
+        project.toDoList[value].urgency = editPriority.value;
+        editPopup.style.display = "none";
+        addListPopup.style.gridRow = "2 / 4";
+        render.refreshList(currentProject);
+    }
 }
-
 function deleteEventListener(element, element2, project, value) {
     element.removeChild(element2);
     project.toDoList.splice(value, 1);
-    refreshList(project);
+    render.refreshList(project);
 }
 
-function clearList() {
-    while (listMain.firstChild) {
-        listMain.removeChild(listMain.lastChild);
-    }
-}
-
-function clearButtons() {
-    while (sidebar.firstChild) {
-        sidebar.removeChild(sidebar.lastChild);
-    }
-}
-
-projectButtons();
+render.projectButtons();
 console.log(projectList);
